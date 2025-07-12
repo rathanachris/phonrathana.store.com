25f9218a97108519324484981b67c6318b3b1155
rathanachris <rathanachris239@gmail.com>
2025-07-13 02:02:52 +0700

    Create script.js
    Signed-off-by: rathanachris <rathanachris239@gmail.com>

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+ <script type="text/javascript">
+		$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 100,
+	itemHeight: 100,
+    itemMargin: 5,
+    minItems: 2,
+    maxItems: 8,
+  })
+});
+</script>
