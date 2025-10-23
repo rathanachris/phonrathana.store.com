1afeb0055c5dded1c72b2a53a2cf2c9091236ec7
chris rathana <rathanachris239@gmail.com>
2025-10-24 02:16:09 +0700

    Update and rename app.js to Key.js
    Signed-off-by: chris rathana <rathanachris239@gmail.com>

--- javascript/Key.js
+++ javascript/Key.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log("Firebase Key:", process.env.FIREBASE_API_KEY);
-console.log("Mongo URI:", process.env.MONGO_URI);
+console.log("odserver URI:", process.env.MONGO_URI);
