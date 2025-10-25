1b824d1f191c7d2419d3078e6292a1066c591efa
chris rathana <rathanachris239@gmail.com>
2025-10-25 14:05:06 +0700

    Create Next.jsx

---
+++ javascript/Next.jsx
@@ -0,0 +1,9 @@
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
