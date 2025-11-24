1038f581980a42ae4577aacaff1f7528eaaed7cd
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-11-24 16:06:42 +0700

    Update and rename System/Control.java to system32/config/system/Control.java
    Signed-off-by: chris rathana <209274714+rathanachris@users.noreply.github.com>

--- system32/config/system/Control.java
+++ system32/config/system/Control.java
@@ -1,7 +1,7 @@
 package com.phonrathana.demo;
 
 import javax.print.attribute.standard.MediaSize.Other;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.webapk.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
