d57ab8ae84dc219961db09a3cde5368eeb63b9e2
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-11-26 05:57:22 +0700

    Update Control.java

--- system32/config/system/Control.java
+++ system32/config/system/Control.java
@@ -4,7 +4,7 @@ import javax.print.attribute.standard.MediaSize.Other;
 import org.springframework.webapk.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("http://api")
 public class OrderController {
 
 // GET
