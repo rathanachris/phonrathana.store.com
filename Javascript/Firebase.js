8d02b4a9fe47ac67532b4e715e5c21f97150e625
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-12-07 10:41:43 +0700

    Delete Javascript directory
    Signed-off-by: chris rathana <209274714+rathanachris@users.noreply.github.com>

--- Javascript/Firebase.js
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-import Odserver from "net";
-dotenv.config("https://$host$request_uri;443");
-
-console.log("Firebase Key:", process.env.FIREBASE_API_KEY);
-console.log("odserver URI:", process.env.MONGO_URI);
