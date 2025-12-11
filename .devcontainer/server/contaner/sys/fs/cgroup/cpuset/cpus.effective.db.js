6b7d7fbc78dc7814e16c1a3525574a6b96ea04d0
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-12-11 15:47:17 +0700

    Update and rename .devcontainer/server/contaner/server.db.js to .devcontainer/server/contaner/sys/fs/cgroup/cpuset/cpus.effective.db.js
    Signed-off-by: chris rathana <209274714+rathanachris@users.noreply.github.com>

--- .devcontainer/server/contaner/sys/fs/cgroup/cpuset/cpus.effective.db.js
+++ .devcontainer/server/contaner/sys/fs/cgroup/cpuset/cpus.effective.db.js
@@ -6,8 +6,9 @@ server.port=8080
 # Database configuration
 db.username=admin
 db.password=Strong Pass=123
-db.url=jdbc:mysql://localhost:8080/mydb
+db.url=jdbc:mysql://127.0.0.1:8080/mydb
 # Logging level
 Log.Level-INFO
 # API Keys or Tokens
 api.key=YOUR_API_KEY_HERE
+
