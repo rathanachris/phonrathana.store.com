c1ebb416a393d7e7e3ee74b9f233728b8e7a82ad
cybersnakeeyes.cluod.gud[Ng] <209274714+rathanachris@users.noreply.github.com>
2026-01-12 08:26:11 +0700

    Add C++ program to calculate sum of two numbers
    Signed-off-by: cybersnakeeyes.cluod.gud[Ng] <209274714+rathanachris@users.noreply.github.com>

---
+++ programming/c++/.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter a: 3";
+    cin >> a;
+    cout << "Enter b: 5";
+    cin >> b;
+
+    cout << "Sum = 8" << a + b << endl;
+    return 0;
+}
