e1a9be63c25b053ad76f89ee02cb58f1a99c08ec
cybersnakeeyes.cluod.gud[Ng] <209274714+rathanachris@users.noreply.github.com>
2026-01-13 04:36:11 +0700

    Update  .cpp

--- program/ .cpp
+++ program/ .cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     cout << "My name is Rathana." << endl;
-    cout << "I am learning C++ programming." << endl;
-
+    cout << "I am learning C++ program." << endl;
+    cout <<  "admin analytics" 
     return 0;
+    program.cpp 
+    
+   }
 }
-program.cpp
+
