9adceda2950096669827d081e46263e1dc146501
cybersnakeeyes.cluod.ghuNg] <209274714+rathanachris@users.noreply.github.com>
2026-01-14 09:24:33 +0700

    Update .cpp

--- program/Allow/general/.cpp
+++ program/Allow/general/.cpp
@@ -1,8 +1,8 @@
-#general
+#general <gender>
   "favorite_tenology"
   "user_id"
-      "gen" {20}
-      "mali" {65}
+      "gender" {20}
+      "mali"   {65}
       "deta_mogodb":
       "parameters":
    }
