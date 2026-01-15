ca2648230f4ee8e51d163168b1539456fdb81871
cybersnakeeyes.cluod.ghuNg] <209274714+rathanachris@users.noreply.github.com>
2026-01-16 00:29:20 +0700

    Create consent/fife.Api.js

---
+++ main.js/consent/fife.Api.js
@@ -0,0 +1,14 @@
+{
+ consentstring: "IAB TCF consent string", 
+ uspstring: "IAB US Privacy String", 
+ gdprApplies: true/false,
+ hasGlobalScope: true/false, 
+ tcfversion: 0/1/2, 
+ tcfcompliant: true/false, 
+ regulation: 0/1/2 (none / GDPR / CCPA), 
+ purposeConsents: {},
+ vendorConsents: {}, 
+ purposeLI: {}, 
+ vendorLI: {}
+ googleVendorConsents: {}
+} 
