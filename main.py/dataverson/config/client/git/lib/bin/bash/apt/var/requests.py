ddd1ed447118460aa499355f0b35f662bca31107
cybersnakeeyes.cluod.ng <209274714+rathanachris@users.noreply.github.com>
2026-01-22 20:47:44 +0700

    Create requests.py
    Signed-off-by: cybersnakeeyes.cluod.ng <209274714+rathanachris@users.noreply.github.com>

---
+++ main.py/dataverson/config/client/git/lib/bin/bash/apt/var/requests.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://www.dns.chrisshop.com/domain"
+output_file = "hosts_filter.txt"
+
+try:
+    response = requests.get(url, verify=False)  # បិទការត្រួតពិនិត្យ SSL
+    response.raise_for_status()  # ពិនិត្យបើមាន error (ដូចជា 503)
+    
+    with open(output_file, "w") as file:
+        file.write(response.text)
+    
+    print(f"ទាញយកជោគជ័យ! ឯកសារត្រូវបានរក្សាទុកជា {output_file}")
+except requests.exceptions.RequestException as e:
+    print(f"មានបញ្ហា: {e}")library
