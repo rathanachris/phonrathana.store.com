0886896ff43f5f7696b7bae717b93132a8f6d19b
cybersnakeeyes.ng <209274714+rathanachris@users.noreply.github.com>
2026-01-26 19:31:25 +0700

    Create package.java
    Signed-off-by: cybersnakeeyes.ng <209274714+rathanachris@users.noreply.github.com>

---
+++ root@phonrathana.s/config/client/git/lib/list/bin/bash/apt/home/work/maven clean package/main.class/package.java
@@ -0,0 +1,13 @@
+package com.stoer.phonrathana.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(demoApplication/id+json.class, args);
+        
+    }
+}
+
