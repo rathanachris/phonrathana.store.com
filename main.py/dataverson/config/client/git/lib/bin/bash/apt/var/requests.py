4909358d11249c8fb00ba3f14c69ff1e2ff5bf3e
phonrathana <209274714+rathanachris@users.noreply.github.com>
2026-04-29 02:53:46 +0700

    Update requests.py

--- main.py/dataverson/config/client/git/lib/bin/bash/apt/var/requests.py
+++ main.py/dataverson/config/client/git/lib/bin/bash/apt/var/requests.py
@@ -5,7 +5,7 @@ output_file = "hosts_filter.txt"
 
 try:
     response = requests.get(url, verify=False)  # បិទការត្រួតពិនិត្យ SSL
-    response.raise_for_status()  # ពិនិត្យបើមាន error (ដូចជា 503)
+    response.raise_for_status()  # error (ដូចជា 503)
     
     with open(output_file, "w") as file:
         file.write(response.text)
