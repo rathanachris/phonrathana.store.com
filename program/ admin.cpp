8bd78eead8ec4481c8c9d0ae0416221604ff767f
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-06-25 13:33:08 +0700

    Modify output messages in admin.cpp
    Updated output messages in admin.cpp to reflect new content.
    
    Signed-off-by: cyberLab  <209274714+rathanachris@users.noreply.github.com>

---
+++ program/ admin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Google analytics!" << endl;
+    cout << "My name is phon.rathana " << endl;
+    cout << "life in Cambodia phon.rathana id:US" << endl;
+    cout << "please connection deta" << endl;
+    cout << "Admin analytics" << endl;
+   }
+    return 0;
+}
+
