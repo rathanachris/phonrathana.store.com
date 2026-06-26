10b7963489df57c464108e46991605c1f8955b53
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-06-26 19:43:05 +0700

    Update until.py

--- main.py/__init__.py
+++ main.py/__init__.py
@@ -1,6 +1,6 @@
 from utils import load_json, log, validate_keys
 
 config = load_json("../config/github.json")
-validate_keys(config, ["api_url", "token", "username"])
+validate_keys(config, ["api_url", "token", "username.txt"])
 
 log("GitHub config loaded successfully")
