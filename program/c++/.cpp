25670e11d8d4286057c2e8ac2594052e46fb211d
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-07-02 22:09:03 +0700

    Modify input prompts and output in main function
    Updated input prompts and output message in main function.
    
    Signed-off-by: cyberLab  <209274714+rathanachris@users.noreply.github.com>

--- program/c++/.cpp
+++ program/c++/.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter a: 3";
-    cin >> a;
-    cout << "Enter b: 5";
-    cin >> b;
+    cout << "hello Google"
+    cin >>  "pls help connect"
+    cout << "address: Cambodia treethome 16 street 4 Dangkor  Dangkor";
+    cin >>  "phone number: 
 
     cout << "Sum = 8" << a + b << endl;
     return 0;
