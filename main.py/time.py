7e9eb80015491be8aa239c541451e58f5dbeff51
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-07-09 02:17:31 +0700

    Create time.py

---
+++ main.py/time.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import time
+
+
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "Current Time:" + time.asctime(t)
+print(f"dear'👿👿')
+print(f"hello.Where is the internetWhere is the internet??")
